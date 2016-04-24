14104f1c3a4228450984238af20012573df81551
bote795 <nbotell2@gmail.com>
2016-04-24 16:01:47 -0400

    remove unique username field

--- models/users/user.js
+++ models/users/user.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 var User = new Schema({
-	username: { type: String, required: true, index: { unique: true }},
+	username: { type: String, required: true},
 	email: { type: String, required: true, index: { unique: true }},
 	password: String,
     image: {type:String, required: true} 
