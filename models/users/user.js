d51f0244d5fa8e3bf4861aaa5ecf1dc9a57b51eb
bote795 <nbotell2@gmail.com>
2016-04-24 15:48:54 -0400

    remove fields we will not be using

--- models/users/user.js
+++ models/users/user.js
@@ -5,31 +5,16 @@ var User = new Schema({
 	username: { type: String, required: true, index: { unique: true }},
 	email: { type: String, required: true, index: { unique: true }},
 	password: String,
-    image: { 
-     	type: mongoose.Schema.Types.ObjectId, 
-     	ref: 'Image'},
-    bio: String,
-    avatars : [{ 
-     	type: mongoose.Schema.Types.ObjectId, 
-     	ref: 'Image'}],
+    image: {type:String, required: true} 
+     	,
 	following: [{type: mongoose.Schema.Types.ObjectId,
         ref: 'Account'}],
 	followers: [{type: mongoose.Schema.Types.ObjectId,
         ref: 'Account'}],
-	resetPasswordToken: String,
-  	resetPasswordExpires: Date,
-    activateAccountToken: String,
-    activateAccountExpires: Date,
-    accountActivated: {type: Boolean, required: true, default: false},
     facebook: {
         id: String,
         token: String
     },
-    google : {
-        id: String,
-        token: String
-    }
-
 });
 
 User.plugin(passportLocalMongoose);
